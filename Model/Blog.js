a5b31074e6ca2984fe8154f53c411bb1ebf07767
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-13 06:51:27 -0700

    Add files via upload

--- Model/Blog.js
+++ Model/Blog.js
@@ -4,7 +4,6 @@ const blog = new mongoose.Schema({
     Description : {type : mongoose.SchemaTypes.String , required : true},
     userId : {type : mongoose.SchemaTypes.ObjectId , ref :"faiz"},
 
-    
 })
 const model1 =  mongoose.model('Blog',blog)
 module.exports= model1
