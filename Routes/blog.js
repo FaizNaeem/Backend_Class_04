09475c350b01964069a5dda5a439e9ba420e3d02
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-13 06:53:14 -0700

    Add files via upload

---
+++ Routes/blog.js
@@ -0,0 +1,81 @@
+const app = require("express")
+const router = app.Router()
+const userModel = require('../Model/faiz')
+const { model } = require("mongoose")
+const user = [
+    {
+    Name:"faiz",
+    id:1
+},
+{
+    Name:"Junaid",
+    id:2
+},
+{
+    Name:"Atta",
+    id:3
+},
+{
+    Name:"Bilal",
+    id:4
+},
+]
+router.get('/', async(req, res)=>{
+    let user1 = await  userModel.find()
+   
+res.status(200).send({
+    status:200,
+    massage:"All users",
+   user1
+})
+})
+router.post('/', async(req, res) => {
+    try{
+        let user2 = await  userModel.create({...req.body})
+        res.status(200).send({ status: 200, user2})
+    }
+    catch(err){
+        res.status(500).send({ status: 500,error:true, msg :"invaid "})
+    }
+router.get('/:id',async(req, res)=>{
+    let user3 = await  userModel.findById(req.params.id)
+    res.status(200).send({
+            status:200,
+            user3
+        }
+    )
+})
+    
+
+})
+router.delete('/:id', async(req, res) => {
+  try{
+await userModel.findByIdAndDelete(req.params.id)
+res.status(200).send({
+    msg:"delete success",
+    status:200
+})
+  }
+  catch(err){
+    res.status(400).send({
+        msg:"Eroor",
+        status:400
+    })
+  }
+})
+router.put('/:id', async(req, res) => {
+    const save = await userModel.findByIdAndUpdate(req.params.id,{...req.body})
+if(!user){
+    res.status(400).send({
+        msg:"Eroor",
+        status:400
+    })
+}
+else{
+    res.status(200).send({
+       save,
+        status:200
+    })
+}
+})
+module.exports= router
