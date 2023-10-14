d164fd02f8e0c006eab25438b419b875f9f46f0c
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-14 20:55:30 +0500

    Update Blog.js

--- Model/Blog.js
+++ Model/Blog.js
@@ -3,7 +3,6 @@ const blog = new mongoose.Schema({
     title : {type : mongoose.SchemaTypes.String , required : true},
     Description : {type : mongoose.SchemaTypes.String , required : true},
     userId : {type : mongoose.SchemaTypes.ObjectId , ref :"faiz"},
-
 })
 const model1 =  mongoose.model('Blog',blog)
 module.exports= model1
