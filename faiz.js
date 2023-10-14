2ebd0d7fda7f87afe4429f951ce4ad1c7d86ecce
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-14 20:53:52 +0500

    Update faiz.js

--- faiz.js
+++ faiz.js
@@ -3,6 +3,5 @@ const faizScheme = new mongoose.Schema({
     title : {type : mongoose.SchemaTypes.String , required : true},
     Description : {type : mongoose.SchemaTypes.String , required : true},
 })
-
 const model1 =  mongoose.model('faiz',faizScheme)
 module.exports= model1
